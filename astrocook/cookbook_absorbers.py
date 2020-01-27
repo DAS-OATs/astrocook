Fixed CCF computation

astrocook/cookbook_absorbers.py
@@ -42,29 +42,32 @@ class CookbookAbsorbers(object):
         self._guess_f = interp1d(ynorm_list, logN_list-0.5, kind='cubic')
 
 
-    def _mod_ccf(self, mod, eval=None, verbose=True):
+    def _mod_ccf(self, mod, ym=None, y=None, verbose=True):
         if eval is None:
-            eval = mod.eval(x=mod._xf, params=mod._pars)
+            ym = mod.eval(x=mod._xf, params=mod._pars)
+        if y is None:
+            y = mod._yf
 
-        w = np.abs(np.gradient(eval))
-        w = w/np.sum(w)
+        #w = np.abs(np.gradient(eval))
+        #w = w/np.sum(w)
         #ccf = np.correlate(eval, mod._yf)[0]
 
+        #mod._yf = mod.eval(x=mod._xf, params=mod._pars)
+
         #ccf_same = np.correlate(eval, mod._yf, mode='same')
-        ccf_same = np.dot(eval, mod._yf)
-        ccf_loc = np.argmax(ccf_same)
-        ccf = np.max(ccf_same)
-        ccf = np.sum(ccf_same)
-        #plt.plot(mod._xf, mod._yf)
-        #plt.plot(mod._xf, eval)
-        #plt.plot(mod._xf, ccf_same)
+        #ccf_loc = np.argmax(ccf_same)
+        #ccf = np.max(ccf_same)
+        ccf = np.dot(ym, y)
+        #ccf = np.corrcoef(eval, mod._yf)[1][0]
+        #plt.plot(mod._xf, y)
+        #plt.plot(mod._xf, ym)
         #plt.scatter(mod._xf[ccf_loc], ccf_same[ccf_loc])
         if verbose:
             logging.info("The data-model CCF is %2.3f." % ccf)
         return ccf
 
 
-    def _mod_ccf_max(self, mod, vstart=-20, vend=20, dv=0.01, verbose=True):
+    def _mod_ccf_max(self, mod, vstart=-20, vend=20, dv=1e-2, verbose=True):
         sd = -1*int(np.floor(np.log10(dv)))-1
 
         xmin = mod._xf[0]
@@ -78,19 +81,24 @@ class CookbookAbsorbers(object):
 
         x_osampl = np.arange(xmin+xstart, xmax+xend, dx)
         eval_osampl = mod.eval(x=x_osampl, params=mod._pars)
-
+        eval_ref = mod.eval(x=mod._xf, params=mod._pars)
         #plt.plot(x_osampl,eval_osampl, linewidth=3)
         #x_shift = np.arange(xstart, xend, dx)
         #print(len(x_shift), len(v_shift))
         ccf = []
+        grad = np.abs(np.gradient(eval_ref))
+        y = (1-mod._yf)*grad/np.sum(grad)
         #plt.plot(mod._xf, mod._yf)
         for xs in x_shift:
             x = x_osampl+xs
             eval = np.interp(mod._xf, x, eval_osampl)
-            ccf1 = self._mod_ccf(mod, eval, verbose=False)
+            grad = np.abs(np.gradient(eval))
+            ym = (1-eval)#*(grad/np.sum(grad))
+            y = (1-mod._yf)#*(grad/np.sum(grad))
+            ccf1 = self._mod_ccf(mod, ym, y, verbose=False)
             ccf.append(ccf1)
-        #amax = np.argmin(ccf)
-        amax = np.argmax(ccf)
+        amax = np.argmin(ccf)
+        #amax = np.argmax(ccf)
         deltax = x_shift[amax]
         deltav = v_shift[amax]
         plt.scatter(xmean+x_shift, ccf/ccf[amax])
@@ -507,7 +515,7 @@ class CookbookAbsorbers(object):
         return 0
 
 
-    def mods_ccf_max(self, vstart=-20, vend=20, dv=1e-2):
+    def mods_ccf_max(self, vstart=-20, vend=20, dv=1):
         """ @brief Maximize data/model CCF
         @details Slide the system models around their mean wavelength to
         determine the best data/model CCF and the corresponding shift.