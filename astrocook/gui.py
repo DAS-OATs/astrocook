c06e495fe4c81da00667f7ef5b3d988c12b85dd8
gcupani <guido.cupani@inaf.it>
2020-05-29 18:00:08 +0200

    Fixed bug in combine sessions

--- astrocook/gui.py
+++ astrocook/gui.py
@@ -409,22 +409,22 @@ class GUIPanelSession(wx.Frame):
             #spec._t = at.vstack([spec._t, self._gui._sess_list[s].spec._t])
 
             for struct in sess_list[s].seq:
-                if hasattr(sess_list[s], struct) \
-                    and getattr(sess_list[s], struct) != None:
+                if getattr(sess_list[s], struct) != None:
                     if struct_out[struct] != None:
                         struct_out[struct]._append(
                             getattr(sess_list[s], struct))
                     else:
                         struct_out[struct] = dc(getattr(sess_list[s], struct))
-    
+
             if name_in[0] == '*':
                 name += '_' + sess_list[s].name
-
 
         struct_out['spec']._t.sort('x')
         if name_in[0] == '*':
             name += name_in[1:]
-        sess = Session(name=name, spec=struct_out['spec'], lines=struct_out['lines'], systs=struct_out['systs'])
+        sess = Session(name=name, spec=struct_out['spec'],
+                       nodes=struct_out['nodes'], lines=struct_out['lines'],
+                       systs=struct_out['systs'])
         return sess
 
 
