86914e490d4db5b6420391dc6d0c107b9bedcbcd
gcupani <guido.cupani@inaf.it>
2020-11-17 12:15:52 +0100

    Fix for Windows paths

--- astrocook/vars.py
+++ astrocook/vars.py
@@ -87,8 +87,8 @@ dy_col_names = np.array(['dy', 'err', 'ERR', 'fluxerr', 'FLUXERR', 'col3'])
 
 h2o_reg = np.array([[1350, 1450], [1800, 1950], [2500, 3400]])
 
-p = '/'.join(os.path.realpath(__file__).split('/')[0:-1]) + '/../'
-atom_par = ascii.read(p+'/atom_par.dat')
+p = '/'.join(pathlib.PurePath(os.path.realpath(__file__)).parts[0:-1]) + '/../'
+atom_par = ascii.read(pathlib.Path(p+'/atom_par.dat'))
 xem_d = {k: v*au.nm for (k, v) in atom_par['col1', 'col2']}
 fosc_d = {k: v for (k, v) in atom_par['col1', 'col3']}
 gamma_d = {k: v for (k, v) in atom_par['col1', 'col4']}
