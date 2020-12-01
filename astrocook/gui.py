8902be914cd7008fa074d5896506d70695d7185e
gcupani <guido.cupani@inaf.it>
2020-12-01 16:21:33 +0100

    Further sanitized paths for Windows

--- astrocook/gui.py
+++ astrocook/gui.py
@@ -332,7 +332,8 @@ class GUIPanelSession(wx.Frame):
         """ Behaviour for Session > Open """
 
         #name = path.split('/')[-1][:-5]
-        name = path.split('/')[-1].split('.')[0]
+        #name = path.split('/')[-1].split('.')[0]
+        name = os.path.realpath(path).split('/')[-1].split('.')[0]
         logging.info("I'm loading session %s..." % path)
         sess = Session(gui=self._gui, path=path, name=name)
         self._gui._panel_sess._on_add(sess, open=True)
