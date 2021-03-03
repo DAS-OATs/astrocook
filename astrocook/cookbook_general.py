Improved function to generate moc spectrum; corrected errors on average in rebinning; various bug fixes

astrocook/cookbook_general.py
@@ -121,7 +121,8 @@ class CookbookGeneral(object):
 
         if new_sess:
             spec_out = dc(spec)
-            spec_out._t = spec._t[mask]
+            spec_out._t['x'][~mask] = np.nan #
+            #spec_out._t = .spec._t[mask]
             from .session import Session
             new = Session(gui=self.sess._gui, name=self.sess.name+'_'+col,
                           spec=spec_out)

astrocook/cookbook_synthetic.py
@@ -14,7 +14,7 @@ class CookbookSynthetic(object):
     def __init__(self):
         super(CookbookSynthetic, self).__init__()
 
-    def spec_from_struct(self, x='0,spec,x', y='0,spec,y', snr=100, ron=0.01):
+    def spec_from_struct(self, x='0,spec,x', y='0,spec,y', dy='0,spec,y', snr=100, ron=0.01):
         """@brief Create a synthetic spectrum
         @details Create a synthetic spectrum from existing structures (a
         wavelenght-like array and a flux-like array). The structure expressions
@@ -46,6 +46,8 @@ class CookbookSynthetic(object):
                                   str(list(np.array(s.spec._t[c]))))
                     y = y.replace('%i,spec,%s' % (i, c),
                                   str(list(np.array(s.spec._t[c]))))
+                    dy = dy.replace('%i,spec,%s' % (i, c),
+                                    str(list(np.array(s.spec._t[c]))))
                     if snr_expr:
                         snr = snr.replace('%i,spec,%s' % (i, c),
                                           str(list(np.array(s.spec._t[c]))))
@@ -56,15 +58,16 @@ class CookbookSynthetic(object):
 
         x = expr_eval(ast.parse(x, mode='eval').body)
         y = expr_eval(ast.parse(y, mode='eval').body)
+        dy = expr_eval(ast.parse(dy, mode='eval').body)
         if snr_expr: snr = expr_eval(ast.parse(snr, mode='eval').body)
 
         xmin, xmax = create_xmin_xmax(x)
-        dy = y/snr
+        #dy = y/snr
 
         # Add gaussian noise
         rng = np.random.default_rng()
         norm = rng.standard_normal(size=y.size)
-        y = y+np.sqrt(dy**2+ron**2)*norm
+        y = y+dy*norm
 
         spec = Spectrum(x, xmin, xmax, y, dy, xunit, yunit)
         from .session import Session

astrocook/functions.py
@@ -51,7 +51,7 @@ def convolve(data, psf):
     s = 0
     l = 0
     for i, k in enumerate(psf):
-        print(i, k)
+        #print(i, k)
         s += l
         l = len(k)
         k_arr = k[np.where(k>0)]
@@ -142,7 +142,7 @@ def expr_check(node):
             ret.append(float(i.n))
         else:
             ret.append(expr_eval(i))
-    return np.ravel(ret)
+    return np.ravel(np.asarray(ret, dtype='float64'))
 
 def expr_eval(node):
     if isinstance(node, ast.Num): # <number>

astrocook/gui.py
@@ -549,7 +549,8 @@ class GUIPanelSession(wx.Frame):
         self._gui._tab_systs.Close()
         self._gui._tab_mods.Close()
         """
-        exit()
+        #exit()
+        os._exit(1)
 
     def _on_deselect(self, event):
         self._sel = event.GetIndex()
@@ -906,7 +907,7 @@ class GUIPanelSession(wx.Frame):
                     expr = expr.replace('%i,spec,%s' % (i, c),
                                         str(list(np.array(s.systs._t[c]))))
 
-        #print(expr)
+        #print(type(expr))
         #print(len(expr))
         #out = expr_eval(ast.parse(expr, mode='eval').body)
 

astrocook/spectrum.py
@@ -134,12 +134,12 @@ class Spectrum(Frame):
             #      for i in range(0, len(spec_x))]
 
             #print('ciao')
-            y1 = y1_osampl[pan:-pan-1]-np.mean(y1_osampl)
-            y2 = y2_osampl[i:-2*pan+i-1]-np.mean(y2_osampl)
+            y1 = y1_osampl[pan:-pan-1]-np.nanmean(y1_osampl)
+            y2 = y2_osampl[i:-2*pan+i-1]-np.nanmean(y2_osampl)
             dy1 = dy1_osampl[pan:-pan-1]
             dy2 = dy2_osampl[i:-2*pan+i-1]
             #print(len(y1_osampl), len(y2_osampl), len(y1), len(y2))
-            ccf.append(np.mean(y2 * y1)/np.sqrt(np.mean(y2**2) * np.mean(y1**2)))
+            ccf.append(np.nanmean(y2 * y1)/np.sqrt(np.nanmean(y2**2) * np.nanmean(y1**2)))
             #ccf.append(np.mean(y2 * y1)/np.sqrt((np.mean(y2**2)-dy2**2) * (np.mean(y1**2)-dy1**2)))
 
         #ccf = ccf/np.max(ccf)
@@ -494,12 +494,17 @@ class Spectrum(Frame):
             #print(frac[w],frac)
             dysel = dy[im:iM]
             if len(frac[w]) > 0:
-                if np.any(np.isnan(dysel)):
+                weights = (frac[w]/dysel[w]**2).value
+                #print(frac[w], np.sum(frac[w])/len(frac[w]))
+                if np.any(np.isnan(dysel)) and False:
                     y_out = np.append(y_out, np.average(ysel[w], weights=frac[w]))
                 else:
-                    y_out = np.append(y_out, np.average(ysel[w], weights=frac[w]/dysel[w]**2))
-                dy_out = np.append(dy_out, np.sqrt(np.sum(frac**2*dysel**2/dysel**4))\
-                                                   /np.sum(frac/dysel**2))
+                    y_out = np.append(y_out, np.average(ysel[w], weights=weights))
+                    #y_out = np.append(y_out, np.average(ysel[w], weights=frac[w]/dysel[w]**2))
+                dy_out = np.append(dy_out, np.sqrt(np.sum(weights**2*dysel[w].value**2))\
+                                                   /np.sum(weights)*y.unit)
+                #dy_out = np.append(dy_out, np.sqrt(np.sum(frac**2/dysel**2))\
+                #                                   /np.sum(frac/dysel**2))
             else:
                 y_out = np.append(y_out, np.nan)
                 dy_out = np.append(dy_out, np.nan)