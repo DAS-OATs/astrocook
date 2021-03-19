Added y scaling to value at a given x; added filling value instead of nan in rebinning; fixed bug in expression parsing

astrocook/cookbook_general.py
@@ -202,7 +202,7 @@ class CookbookGeneral(object):
 
 
     def rebin(self, xstart=None, xend=None, dx=10.0, xunit=au.km/au.s,
-              norm=True):
+              norm=True, filling=np.nan):
         """ @brief Rebin spectrum
         @details Rebin a spectrum with a given step. The step can be expressed
         in any unit of wavelength or velocity. Start and end wavelength may be
@@ -216,6 +216,7 @@ class CookbookGeneral(object):
         @param dx Step in x
         @param xunit Unit of wavelength or velocity
         @param norm Return normalized spectrum, if continuum exists
+        @param filling Value to fill region without data
         @return Session with rebinned spectrum
         """
 
@@ -224,6 +225,7 @@ class CookbookGeneral(object):
             xend = None if xend in [None, 'None'] else float(xend)
             dx = float(dx)
             xunit = au.Unit(xunit)
+            filling = float(filling)
         except ValueError:
             logging.error(msg_param_fail)
             return None
@@ -256,7 +258,7 @@ class CookbookGeneral(object):
             y = spec_in.y/spec_in.t['cont']
             dy = spec_in.dy/spec_in.t['cont']
 
-        spec_out = spec_in._rebin(xstart, xend, dx, xunit, y, dy)
+        spec_out = spec_in._rebin(xstart, xend, dx, xunit, y, dy, filling)
         if cont:
             spec_out.t['cont'] = 1
 
@@ -566,6 +568,29 @@ class CookbookGeneral(object):
         """
 
         fact = 1/np.median(self.sess.spec.y)
+
+        for s in self.sess.seq:
+            try:
+                struct = getattr(self.sess, s)
+                struct._y_scale(fact)
+            except:
+                logging.debug(msg_attr_miss(s))
+        return 0
+
+    def y_scale_x(self, x):
+        """ @brief Scale y axis by its value at a given x
+        @details Scale the y axis by its value at a given x.
+        @param x x (nm)
+        @return 0
+        """
+
+        try:
+            x = float(x)
+        except ValueError:
+            logging.error(msg_param_fail)
+            return 0
+
+        fact = 1/np.interp(x, self.sess.spec.x.to(au.nm).value, self.sess.spec.y)
 
         for s in self.sess.seq:
             try:

astrocook/graph.py
@@ -297,6 +297,9 @@ class Graph(object):
                                 % sess[0].spec._rfz)
             if self._axt == None:
                 self._axt = self._ax.twiny()
+            try:
+                self._axt.set_xlabel(str(self._gui._sess_sel.spec._xunit))
+            except:
                 self._axt.set_xlabel(str(self._xunit))
         else:
             try:
@@ -392,7 +395,7 @@ class Graph(object):
                 sel, struct, xcol, ycol, mcol, mode, style, width, color, alpha\
                     = e.split(',')
                 #print(sel, struct, xcol, ycol, mcol, mode, style, width, color, alpha)
-                #sess = self._gui._sess_list[int(sel)]
+                sess = self._gui._sess_list[int(sel)]
                 #sess = self._gui._sess_sel
                 xunit = sess.spec.x.unit
                 if struct in ['spec','lines','nodes','systs']:
@@ -426,6 +429,7 @@ class Graph(object):
                         x = xem*(1+z_flat/(1+self._gui._sess_sel.spec._rfz))
                     else:
                         x = xem*(1+z_flat)
+                    x = x.to(sess.spec._xunit)
                     #print(graph._xs)
                     #print(self._zems, self._series, self._axes, self._ax)
                     #print(zems)
@@ -438,7 +442,7 @@ class Graph(object):
                         for k in self._axes:
                             if self._axes[k] == self._ax:
                                 zem = self._zems[k]
-                        x = np.log(x.value/((1+zem)*121.567))*aconst.c.to(au.km/au.s)
+                        x = np.log(x.to(au.nm).value/((1+zem)*121.567))*aconst.c.to(au.km/au.s)
                         #print(set(zip(series_flat,x)))
                     self._systs_series = series_flat
                     self._systs_z = z_flat

astrocook/gui.py
@@ -893,18 +893,20 @@ class GUIPanelSession(wx.Frame):
         @return 0
         """
 
-        for i, s in enumerate(self._gui._sess_list):
+        # Reversed to parse sessions with higher number first, and avoid overwriting
+        sess_list = self._gui._sess_list[::-1]
+        for i, s in enumerate(sess_list):
             if s.spec is not None:
                 for c in sorted(s.spec._t.colnames, key=len, reverse=True):
-                    expr = expr.replace('%i,spec,%s' % (i, c),
+                    expr = expr.replace('%i,spec,%s' % (len(sess_list)-1-i, c),
                                         str(list(np.array(s.spec._t[c]))))
             if s.lines is not None:
                 for c in sorted(s.lines._t.colnames, key=len, reverse=True):
-                    expr = expr.replace('%i,spec,%s' % (i, c),
+                    expr = expr.replace('%i,spec,%s' % (len(sess_list)-1-i, c),
                                         str(list(np.array(s.lines._t[c]))))
             if s.systs is not None:
                 for c in sorted(s.systs._t.colnames, key=len, reverse=True):
-                    expr = expr.replace('%i,spec,%s' % (i, c),
+                    expr = expr.replace('%i,spec,%s' % (len(sess_list)-1-i, c),
                                         str(list(np.array(s.systs._t[c]))))
 
         #print(expr)

astrocook/spectrum.py
@@ -445,7 +445,7 @@ class Spectrum(Frame):
 
         return lines
 
-    def _rebin(self, xstart, xend, dx, xunit, y, dy):
+    def _rebin(self, xstart, xend, dx, xunit, y, dy, filling=np.nan):
 
         # Convert spectrum into chosen unit
         # A deep copy is created, so the original spectrum is preserved
@@ -512,8 +512,8 @@ class Spectrum(Frame):
                 #dy_out = np.append(dy_out, np.sqrt(np.sum(frac**2/dysel**2))\
                 #                                   /np.sum(frac/dysel**2))
             else:
-                y_out = np.append(y_out, np.nan)
-                dy_out = np.append(dy_out, np.nan)
+                y_out = np.append(y_out, filling)
+                dy_out = np.append(dy_out, filling)
 
         # Create a new spectrum and convert it to the units of the original one
         out = Spectrum(x, xmin, xmax, y_out, dy_out, xunit=xunit, yunit=y.unit,