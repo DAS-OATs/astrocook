1e52f0ab240e14a4c0057829541a0bd44809d442
gcupani <guido.cupani@inaf.it>
2021-07-05 15:36:37 +0200

    Added support for TNG format. Added possibility to select systems by series

--- astrocook/format.py
+++ astrocook/format.py
@@ -479,7 +479,6 @@ class Format(object):
             return None
 
 
-
     def mage_spectrum(self, hdul):
         """ LDSS3 spectrum """
         logging.info(msg_format('QUBRICS'))
@@ -523,6 +522,25 @@ class Format(object):
             meta['object'] = ''
         """
         return Spectrum(x, xmin, xmax, y, dy, xunit, yunit, meta)
+
+
+    def lrs_spectrum(self, hdul):
+        """ TNG LRS spectrum """
+        logging.info(msg_format('LRS'))
+        hdr = hdul[0].header
+        data = hdul[0].data
+        crval1 = hdr['CRVAL1']
+        cdelt1 = hdr['CDELT1']
+        naxis1 = hdr['NAXIS1']
+        y = data
+        x = np.arange(crval1, crval1+naxis1*cdelt1, cdelt1)[:len(y)]
+        xmin, xmax = self._create_xmin_xmax(x)
+        dy = np.full(len(y), np.nan)
+        xunit = au.Angstrom
+        yunit = au.erg/au.cm**2/au.s/au.Angstrom
+        meta = hdr
+        return Spectrum(x, xmin, xmax, y, dy, xunit, yunit, meta)
+
 
     def uves_spectrum(self, hdul, hdul_err):
         """ UVES format (two files: FLUXCAL_SCI, FLUXCAL_ERRORBAR_SCI) """
