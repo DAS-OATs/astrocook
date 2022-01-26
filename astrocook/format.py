3cd56621dbc0333c3a7d38d2719dfef8ffd1f669
gcupani <guido.cupani@inaf.it>
2022-01-26 15:36:24 +0100

    Added feature boundaries to session structures (also plotted)

--- astrocook/format.py
+++ astrocook/format.py
@@ -523,6 +523,9 @@ class Format(object):
             try:
                 xunit = data_s.__dict__['_coldefs'][x_name]._unit
             except:
+                xunit = None
+
+            if xunit == None:
                 xunit = au.nm
                 if np.nanmax(x)>3000:
                     x = x*0.1
@@ -530,7 +533,6 @@ class Format(object):
                 yunit = au.Unit(data_s.__dict__['_coldefs'][y_name]._unit)
             except:
                 yunit = au.erg/au.cm**2/au.s/au.Angstrom
-
             
             xmin, xmax = self._create_xmin_xmax(x)
             meta = hdr #{}
