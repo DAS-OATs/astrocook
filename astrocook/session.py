3d56a11f2f6097334b746a367f50e852636ac1d6
gcupani <guido.cupani@inaf.it>
2022-02-11 17:00:32 +0100

    feats/ directory now removed upon loading

--- astrocook/session.py
+++ astrocook/session.py
@@ -456,6 +456,7 @@ class Session(object):
         """
         s._load(new_dir)
         setattr(self, struct, s)
+        shutil.rmtree(new_dir, ignore_errors=True)
 
 
     def _save(self, struct, dir, stem, arch):
