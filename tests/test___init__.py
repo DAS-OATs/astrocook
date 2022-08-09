3a5d62a68ccdd3a9b883928239ea71c97ef2b333
gcupani <guido.cupani@inaf.it>
2022-08-09 10:06:08 +0200

    First trials with pytest

---
+++ tests/test___init__.py
@@ -0,0 +1,5 @@
+import pytest
+from astrocook import __init__
+
+def test___init__():
+    pass
---
+++ tests/test_defaults.py
@@ -0,0 +1,34 @@
+import pytest
+import wx
+
+app = wx.App(False)
+from astrocook.gui import GUI
+gui = GUI()
+
+from astrocook.defaults import Defaults, _dict_merge
+
+def test__dict_merge():
+    dict1 = {'a':{'a0':0, 'a1':1}, 'b':{'b0':0, 'b1':1}}
+    dict2 = {'b':{'b2':2, 'b3':3}, 'c':{'c0':0, 'c1':1}}
+    assert _dict_merge(dict1, dict2) == {'a':{'a0':0, 'a1':1},
+                                         'b':{'b0':0, 'b1':1,'b2':2, 'b3':3},
+                                         'c':{'c0':0, 'c1':1}}
+
+class TestDefaults:
+
+    def test_open(self):
+        defs = Defaults(gui)
+        defs.open(path='.', file='test_defaults.json')
+        dict = {'key': 'value--'}
+        dict = _dict_merge(defs._extend, dict)
+        assert defs.dict == dict
+
+    """
+    def test_update(self):
+        defs = Defaults(gui)
+        str = '{"key": "value--"}'
+        defs.update(str)
+        dict = {'key': 'value--'}
+        dict = _dict_merge(defs._extend, dict)
+        assert defs.dict == dict
+    """
