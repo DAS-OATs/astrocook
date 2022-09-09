9370ff2c7c61c28974df9be093b6e55382e67f61
gcupani <guido.cupani@inaf.it>
2022-09-09 15:32:39 +0200

    Added .dat files to the wildcards for opening

--- astrocook/gui_menu.py
+++ astrocook/gui_menu.py
@@ -283,6 +283,7 @@ class GUIMenu(object):
                        "FITS files (*.fits)|*.fits|" \
                        "JSON files (*.json)|*.json|" \
                        "CSV files (*.csv)|*.csv|" \
+                       "Data files (*.dat)|*.dat|" \
                        "Text files (*.txt)|*.txt"
         with wx.FileDialog(self._gui._panel_sess, "Open file", path,
                            wildcard=wildcard,
