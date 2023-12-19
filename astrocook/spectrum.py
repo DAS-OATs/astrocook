7f0f787d82f1e32e241b8adab2bb3266d30a9a53
G-Francio <50723359+G-Francio@users.noreply.github.com>
2023-12-19 13:33:36 +0100

    Rename `_psf_gauss` to `psf_gauss`

--- astrocook/spectrum.py
+++ astrocook/spectrum.py
@@ -769,7 +769,7 @@ class Spectrum(Frame):
 
         return 0
 
-    def _psf_gauss(self, x, resol):
+    def psf_gauss(self, x, resol):
         if resol in self.gauss_psf_cache:
             return self.gauss_psf_cache[resol]
 
@@ -780,7 +780,7 @@ class Spectrum(Frame):
         psf = psf[np.where(psf > 1e-6)]
 
         if len(psf)==0:
-            return self._psf_gauss(self.x.to(xunit_def).value, resol)
+            return self.psf_gauss(self.x.to(xunit_def).value, resol)
         else:
             ret = psf
             self.gauss_psf_cache[resol] = ret
