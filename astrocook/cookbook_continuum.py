4b78e6822383f1c9bd388676a60bf20a20bb6efd
gcupani <guido.cupani@inaf.it>
2024-02-16 16:29:55 +0100

    Added x range to apply continuum

--- astrocook/cookbook_continuum.py
+++ astrocook/cookbook_continuum.py
@@ -407,8 +407,8 @@ class CookbookContinuum(object):
 ### Advanced
 
 
-    def flux_clip(self, hwindow=100, kappa_hi=6, kappa_lo=3, iter=100, std=500,
-                  fudge=1, delta_x=5000):
+    def flux_clip(self, xmin, xmax, hwindow=100, kappa_hi=6, kappa_lo=3,
+                  iter=100, std=500, fudge=1, delta_x=5000):
         """ @brief Clip flux in spectrum
         @details Discriminate absorbed spectrum bins by applying a kappa-sigma
         clipping within a running window.
@@ -438,10 +438,15 @@ class CookbookContinuum(object):
         outliers (absorbed bins), `0` elsewhere. Similarly, a column `y_em` is
         created for the upper outliers (“emitted” bins) and a column `y_cont`
         is created for the remaining bins (continuum bins). The latter is
-        smoothed in the velocity space with a gaussian filter with standard
-        deviation `std`, and multiplied for a `fudge` factor. The result is
-        saved in column `cont` of the spectrum as the current estimate of the
-        emission continuum.
+        multiplied for a `fudge` factor and smoothed in the velocity space with
+        a gaussian filter with standard deviation `std`. The procedure is
+        applied in the range `xmin`-`xmax`, except for the smoothing which is
+        applied to the whole spectral range. The result is saved in column
+        `cont` of the spectrum as the current estimate of the emission
+        continuum. A set of `delta_x`-spaced nodes is superimposed to the final
+        continuum
+        @param xmin Minimum wavelength (nm)
+        @param xmax Maximum wavelength (nm)
         @param hwindow Half-window size in pixels for running mean
         @param kappa_hi Number of standard deviations for clipping above
         @param kappa_lo Number of standard deviations for clipping below
@@ -453,6 +458,8 @@ class CookbookContinuum(object):
         """
 
         try:
+            xmin = 0*au.nm if xmin in ["", "None", None] else float(xmin) * au.nm
+            xmax = 1e4*au.nm if xmax in ["", "None", None] else float(xmax) * au.nm
             hwindow = int(hwindow)
             kappa_hi = float(kappa_hi)
             kappa_lo = float(kappa_lo)
@@ -467,43 +474,46 @@ class CookbookContinuum(object):
         spec = self.sess.spec
         lines = self.sess.lines
 
-        y_rm = running_mean(spec._t['y'], h=hwindow)
+        xsel = np.logical_and(spec._t['x']>xmin, spec._t['x']<xmax)
+        spec_t = spec._t[xsel]
+
+        y_rm = running_mean(spec_t['y'], h=hwindow)
 
         if 'y_rm' not in spec._t.colnames:
             logging.info("I'm adding column 'y_rm'.")
         else:
             logging.warning("I'm updating column 'y_rm'.")
-        spec._t['y_rm'] = at.Column(y_rm, dtype=float)
+        spec_t['y_rm'] = at.Column(y_rm, dtype=float)
 
-        if 'y_em' not in spec._t.colnames:
+        if 'y_em' not in spec_t.colnames:
             logging.info("I'm adding column 'y_em'.")
-        spec._t['y_em'] = at.Column(np.ones(len(spec._t)), dtype=int)
-        if 'y_abs' not in spec._t.colnames:
+        spec_t['y_em'] = at.Column(np.ones(len(spec_t)), dtype=int)
+        if 'y_abs' not in spec_t.colnames:
             logging.info("I'm adding column 'y_abs'.")
-        spec._t['y_abs'] = at.Column(np.zeros(len(spec._t)), dtype=int)
-        if 'y_cont' not in spec._t.colnames:
+        spec_t['y_abs'] = at.Column(np.zeros(len(spec_t)), dtype=int)
+        if 'y_cont' not in spec_t.colnames:
             logging.info("I'm adding column 'y_cont'.")
-        spec._t['y_cont'] = spec._t['y']
-        if 'cont' not in spec._t.colnames:
+        spec_t['y_cont'] = spec_t['y']
+        if 'cont' not in spec_t.colnames:
             logging.info("I'm adding column 'cont'.")
-        spec._t['cont'] = at.Column(np.array(None, ndmin=1), dtype=float)
-        #spec._t['cont'].unit = spec._t['y'].unit
+        spec_t['cont'] = at.Column(np.array(None, ndmin=1), dtype=float)
+        #spec_t['cont'].unit = spec_t['y'].unit
 
-        sum_sel = len(spec._t)
+        sum_sel = len(spec_t)
         for i in range(iter):
-            sel = spec._t['y']-spec._t['y_rm']<-kappa_lo*spec._t['dy']
-            sel = np.logical_or(sel, spec._t['y']-spec._t['y_rm']>kappa_hi*spec._t['dy'])
-            spec._t['y_em'][sel] = 0
-            spec._t['y_abs'][sel] = 1
-            x_rm = spec._t['x'][~sel]
-            if 2*hwindow+1>len(spec._t['y'][~sel]):
+            sel = spec_t['y']-spec_t['y_rm']<-kappa_lo*spec_t['dy']
+            sel = np.logical_or(sel, spec_t['y']-spec_t['y_rm']>kappa_hi*spec_t['dy'])
+            spec_t['y_em'][sel] = 0
+            spec_t['y_abs'][sel] = 1
+            x_rm = spec_t['x'][~sel]
+            if 2*hwindow+1>len(spec_t['y'][~sel]):
                 logging.warning("Too many outliers to compute a running "
                                 "median at iteration %i! Try increasing sigma."
                                 % i)
-                y_rm = np.ones(len(spec._t['y'][~sel])) \
-                       * np.median(spec._t['y'][~sel])
+                y_rm = np.ones(len(spec_t['y'][~sel])) \
+                       * np.median(spec_t['y'][~sel])
             else:
-                y_rm = running_mean(spec._t['y'][~sel], h=hwindow)
+                y_rm = running_mean(spec_t['y'][~sel], h=hwindow)
 
             if i == iter-1 and sum_sel != np.sum(sel):
                 logging.warning("Clipping not converged after %i iterations! "
@@ -514,15 +524,21 @@ class CookbookContinuum(object):
                 logging.info("Clipping converged after %i iterations." % (i+1))
                 break
 
-            spec._t['y_rm'] = np.interp(spec._t['x'], x_rm, y_rm)
-        x_cont = spec._t['x'][np.where(spec._t['y_em'])]
-        y_cont = spec._t['y'][np.where(spec._t['y_em'])]
-        spec._t['y_cont'] = np.interp(spec._t['x'], x_rm, y_rm)*spec._t['y'].unit
+            spec_t['y_rm'] = np.interp(spec_t['x'], x_rm, y_rm)
+        x_cont = spec_t['x'][np.where(spec_t['y_em'])]
+        y_cont = spec_t['y'][np.where(spec_t['y_em'])]
+        spec_t['y_cont'] = np.interp(spec_t['x'], x_rm, y_rm)*spec_t['y'].unit
         if lines is not None:
             lines._t['y_cont'] = np.interp(lines._t['x'], x_rm, y_rm)*lines._t['y'].unit
 
+        spec_t['y_cont'] *= fudge
+        if 'cont' in spec._t.colnames:
+            spec._t['y_cont'] = spec._t['cont']
+        else:
+            spec._t['y_cont'] = np.nan
+        spec._t['y_cont'][xsel] = spec_t['y_cont']
+
         spec._gauss_convolve(std=std, input_col='y_cont', output_col='cont')
-        spec._t['cont'] *= fudge
         self.nodes_extract(delta_x=delta_x, mode='cont')
 
         return 0
