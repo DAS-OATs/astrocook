dd4b8d0284706fc067f388daabda44fe6bd3c390
gcupani <guido.cupani@inaf.it>
2024-03-05 21:55:39 +0100

    Added new set of column names to the inventory for input spectra

--- astrocook/vars.py
+++ astrocook/vars.py
@@ -144,9 +144,9 @@ psf_gauss_d = {
 forbidden_keywords = ['XTENSION', 'BITPIX', 'PCOUNT', 'GCOUNT', 'TFIELDS',
                       'NAXIS', 'TTYPE', 'TFORM', 'TUNIT', 'TDISP']
 
-x_col_names = np.array(['x', 'wave', 'WAVE', 'col1', 'lambda', 'wavelength', 'OPT_WAVE'])
-y_col_names = np.array(['y', 'flux', 'FLUX', 'col2', 'flux_cal', 'OPT_COUNTS'])
-dy_col_names = np.array(['dy', 'err', 'ERR', 'fluxerr', 'FLUXERR', 'error', 'ERROR', 'col3', 'error_cal', 'sigma', 'OPT_COUNTS_SIG'])
+x_col_names = np.array(['x', 'wave', 'WAVE', 'col1', 'lambda', 'wavelength', 'OPT_WAVE', 'Wavelength(AA)'])
+y_col_names = np.array(['y', 'flux', 'FLUX', 'col2', 'flux_cal', 'OPT_COUNTS', 'Flux(erg/s/cm2/AA)'])
+dy_col_names = np.array(['dy', 'err', 'ERR', 'fluxerr', 'FLUXERR', 'error', 'ERROR', 'col3', 'error_cal', 'sigma', 'OPT_COUNTS_SIG', 'eFlux(erg/s/cm2/AA)'])
 cont_col_names = np.array(['cont', 'CONT', 'Continuum', 'col4'])
 
 h2o_reg = np.array([[1350, 1450], [1800, 1950], [2500, 3400]])
