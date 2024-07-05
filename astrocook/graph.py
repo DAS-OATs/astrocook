fccd07b77e97fdd1361a7cca34c9e452359f00b0
gcupani <guido.cupani@inaf.it>
2024-07-05 16:10:36 +0200

    Fixed two little bugs with system cursor

--- astrocook/graph.py
+++ astrocook/graph.py
@@ -244,20 +244,19 @@ class Graph(object):
                     focus._textbar.SetLabel("x=%2.4f, y=%2.4e; z[%s]=%2.5f" \
                                             % (x, y, self._cursor._series, z_obs))
             else:
-
                 if sess.spec._xunit.is_equivalent(au.nm):  # X-axis in wavelengths
                     z = x/x_convert(self._cursor._xmean, sess.spec._zem, sess.spec._xunit).value-1
-                    xzs = [x_convert(xem*au.nm, sess.spec._zem, sess.spec._xunit)*(1+z)
+                    xzs = [x_convert(xem*au.nm, sess.spec._zem, sess.spec._xunit).value*(1+z)
                             for xem in self._cursor._xem]
                 else:  # X-axis in velocities
                     xc = x_convert(x*sess.spec._xunit, sess.spec._zem,
                                    au.nm).value
                     z = xc/x_convert(self._cursor._xmean, sess.spec._zem, au.nm).value-1
-                    xzs = [x_convert(xem*au.nm*(1+z), sess.spec._zem, sess.spec._xunit)
+                    xzs = [x_convert(xem*au.nm*(1+z), sess.spec._zem, sess.spec._xunit).value
                            for xem in self._cursor._xem]
                 for c, xz in zip(self._cursor_line, xzs):
                     #c.set_xdata((xem*(1+z)*au.nm).to(sess.spec._xunit))
-                    c.set_xdata(xz.value)
+                    c.set_xdata([xz])
                     c.set_alpha(0.5)
                 z_obs = z*(1+sess.spec._rfz)
                 focus._textbar.SetLabel("x=%2.4f, y=%2.4e; z[%s]=%2.5f" \
