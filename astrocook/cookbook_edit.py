65c8b7cc6aa4d5e5de80e41dd40df90e6423b8c2
gcupani <guido.cupani@inaf.it>
2024-08-29 16:55:56 +0200

    First working version (condition still not working)

--- astrocook/cookbook_edit.py
+++ astrocook/cookbook_edit.py
@@ -41,10 +41,28 @@ class CookbookEdit(CookbookEditOld):
         return 0
 
 
-    def mask(self):
-        """@brief Mask 🚧
-        @details 🚧
+    def mask(self, shift=0, tell=True, sky=True, cond=''):
+        """@brief Mask
+        @details Mask telluric lines, sky lines, or spectral regions defined by
+        a condition
         @url other_cb.html#mask
+        @param shift Shift to the barycentric frame (km/s)
+        @param tell Mask telluric lines
+        @param sky Mask sky lines
+        @param cond Condition
+        @return 0
         """
+
+        try:
+            shift = float(shift)
+            tell = str(tell) == 'True'
+            sky = str(sky) == 'True'
+        except:
+            logging.error(msg_param_fail)
+            return 0
+
+        if tell: self.telluric_mask(shift=shift)
+        if sky: self.sky_mask(shift=shift)
+        if cond!='': self.mask_cond(cond=cond, new_sess=False)
 
         return 0
