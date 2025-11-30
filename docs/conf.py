fcc9edcc53a6b458a23df57dc70e77ec1b88dfb2
Guido Cupani <guido.cupani@inaf.it>
2025-11-30 15:11:27 +0100

    feature(docs): Updated webpage appearance

--- docs/conf.py
+++ docs/conf.py
@@ -10,16 +10,12 @@ import sys
 sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 project = 'Astrocook'
 copyright = '2025, Guido Cupani'
 author = 'Guido Cupani'
 release = '2.0.0'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
 extensions = [
     'sphinx.ext.autodoc',      # Core library for html generation from docstrings
     'sphinx.ext.autosummary',  # Create neat summary tables
@@ -34,7 +30,19 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'alabaster'
+html_theme = 'pydata_sphinx_theme'
 html_static_path = ['_static']
+
+# -- MyST Parser Configuration -----------------------------------------------
+# This allows you to use colon fences for directives (like ::: note)
+myst_enable_extensions = [
+    "colon_fence",
+    "dollarmath",
+]
+
+# -- PyData Theme Configuration ----------------------------------------------
+html_theme_options = {
+    "github_url": "https://github.com/das-oats/astrocook",
+    "show_nav_level": 2,
+    "navigation_depth": 4,
+}
